b321276946254356e4f59b0cffa421211117ede7
Oriel Sanchez <orielbenitosanchez@gmail.com>
2023-05-30 13:37:03 -0700

    fixed treesitter and lsp

--- .config/nvim/lua/core/plugin_config/treesitter.lua
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,8 +1,17 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "rust", "vim", "python" },
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = "all",
   sync_install = false,
-  auto_install = true,
+  ignore_install = { "" },
   highlight = {
     enable = true,
+    disable = { "" },
+    additional_vim_regex_highlighting = true,
+
   },
+  indent = { enable = true, disable = { "yaml" } },
 }
